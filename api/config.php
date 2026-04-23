62ddf7fd3eb22cfb5811ed70e59742a0f64d6b5a
Robiul Islam <141323889+dev3ROBI@users.noreply.github.com>
2026-04-23 15:59:13 +0600

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,35 +1,13 @@
 <?php
 
 // =======================
-// STATIC DATABASE CONFIG
+// DATABASE CONFIG
 // =======================
 define('DB_HOST', 'shortline.proxy.rlwy.net');
 define('DB_NAME', 'railway');
 define('DB_USER', 'root');
 define('DB_PASS', 'fjEvGLOytnFmjDPIOXbVtrpLMIGotaFv');
 define('DB_PORT', 14989);
-
-// =======================
-// MYSQL CONNECTION
-// =======================
-$conn = new mysqli(
-    DB_HOST,
-    DB_USER,
-    DB_PASS,
-    DB_NAME,
-    DB_PORT
-);
-
-if ($conn->connect_error) {
-    http_response_code(500);
-    die(json_encode([
-        "status" => "error",
-        "message" => "DB Connection failed",
-        "error" => $conn->connect_error
-    ]));
-}
-
-$conn->set_charset("utf8mb4");
 
 // =======================
 // GOOGLE OAUTH
