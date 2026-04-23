455946787e41abc1fa3c8d9a9d0f692a620c9f15
Robiul Islam <141323889+dev3ROBI@users.noreply.github.com>
2026-04-23 13:01:40 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
 
+echo json_encode([
+    "status" => "success",
+    "message" => "Shikhbo API is running successfully 🚀",
+    "version" => "1.0",
+    "endpoints" => [
+        "login" => "api/login.php",
+        "signup" => "api/signup.php",
+        "google_login" => "api/google_login.php",
+        "get_app_settings" => "api/get_app_settings.php"
+    ],
+    "server_time" => date("Y-m-d H:i:s")
+]);
